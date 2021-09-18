3ddfae1a503e6721ef48dd173b777c54cf91f9e2
Shivashish Yadav <shivashishmca@gmail.com>
2021-09-18 16:22:57 +0530

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: `Shivashish`,
+    description: `Indian Food`,
+    author: "Shivashish",
+    name: `Shivashish`,
+    tagline: `Indian Food`,
+    social: [
+      {
+        name: `Facebook`,
+        url: `https://facebook.com`,
+      },
+      {
+        name: `Twitter`,
+        url: `https://twitter.com`,
+      },
+      {
+        name: `Instagram`,
+        url: `https://instagram.com`,
+      },
+    ],
+  },
+  plugins: [
+  
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Shivashish`,
+        short_name: `Shivashish`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#000`,
+        icon: `content/assets/icon.png`,
+        display: `standalone`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-serena`,
+      options: {},
+    },
+    
+  ],
+}
